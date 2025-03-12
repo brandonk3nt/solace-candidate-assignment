6f63028ca2950b50036dd095ecc190de4cc6eb24
Brandon Kent <brandonk3nt@gmail.com>
2025-03-11 23:38:19 -0700

    Abstract advocate table

---
+++ src/app/components/AdvocateTable.tsx
@@ -0,0 +1,33 @@
+import { Advocate } from "../types";
+import AdvocateTableRow from "./AdvocateTableRow";
+
+type AdvocateTableProps = {
+  advocates: Advocate[];
+};
+
+const headerLabels = [
+  "First Name",
+  "Last Name",
+  "City",
+  "Degree",
+  "Specialties",
+  "Years of Experience",
+  "Phone Number",
+];
+
+export default function AdvocateTable({ advocates }: AdvocateTableProps) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headerLabels.map((header, i) => <th key={i}>{header}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {advocates.map((advocate, i) => (
+          <AdvocateTableRow key={i} advocate={advocate} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
